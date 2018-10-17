355e3d5bf4d624df0f526340102c915ea9332dae
Maksym Skliarov <maksym.skliarov@gmail.com>
2018-10-17 16:48:21 +0300

    Version 0.3.0

--- Quack/Classes/HoverViewController.swift
+++ Quack/Classes/HoverViewController.swift
@@ -26,7 +26,7 @@ internal class HoverViewController: UIViewController {
     /**
         Spinner in the center
     */
-  fileprivate let activityIndicatorView = UIActivityIndicatorView(style: .white)
+  fileprivate let activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .white)
     /**
         UILabel under spinner
     */
@@ -84,9 +84,9 @@ internal class HoverViewController: UIViewController {
     
     fileprivate func addSpinner() {
         if self.hoverStyle == .light {
-          self.activityIndicatorView.style = .gray
+          self.activityIndicatorView.activityIndicatorViewStyle = .gray
         } else {
-          self.activityIndicatorView.style = .white
+          self.activityIndicatorView.activityIndicatorViewStyle = .white
         }
         self.activityIndicatorView.alpha = 0.0
         self.activityIndicatorView.translatesAutoresizingMaskIntoConstraints = false
--- Quack/Classes/Quack.swift
+++ Quack/Classes/Quack.swift
@@ -8,96 +8,96 @@
 import UIKit
 
 open class Quack {
+  
+  /**
+    Singleton instance
+   */
+  fileprivate static let shared = Quack()
+  
+  /**
+    Lazy initialize hover window and keep strong reference on it
+   */
+  fileprivate lazy var hoverWindow: UIWindow = {
+    let window = UIWindow(frame: UIScreen.main.bounds)
+    window.backgroundColor = UIColor.clear
+    window.windowLevel = UIWindowLevelStatusBar - 1
+    window.screen = UIScreen.main
+    return window
+  }()
+  
+  /**
+   Present UIAlertController with title, message and "OK" button from specified controller
+   */
+  open class func showAlert(title: String?, message: String?, inController presentingController: UIViewController) {
     
-    /**
-        Singleton instance
-    */
-    fileprivate static let shared = Quack()
+    // Create alert
+    let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "OK button title in alerts"), style: .default, handler: nil))
     
-    /**
-        Lazy initialize hover window and keep strong reference on it
-    */
-    fileprivate lazy var hoverWindow: UIWindow = {
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.backgroundColor = UIColor.clear
-      window.windowLevel = UIWindow.Level.statusBar - 1
-        window.screen = UIScreen.main
-        return window
-    }()
-    
-    /**
-        Present UIAlertController with title, message and "OK" button from specified controller
-    */
-    open class func showAlert(title: String?, message: String?, inController presentingController: UIViewController) {
-        
-        // Create alert
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "OK button title in alerts"), style: .default, handler: nil))
-        
-        // Present alert
-        presentingController.present(alertController, animated: true, completion: nil)
+    // Present alert
+    presentingController.present(alertController, animated: true, completion: nil)
+  }
+  
+  /**
+   Present light transparent hover with spinner
+   */
+  open class func showLightHover() {
+    Quack.shared.hoverWindow.rootViewController = HoverViewController(hoverStyle: .light)
+    Quack.shared.hoverWindow.makeKeyAndVisible()
+  }
+  
+  /**
+   Present light transparent hover with spinner and text
+   */
+  open class func showLightHover(text: String) {
+    Quack.shared.hoverWindow.rootViewController = HoverViewController(hoverStyle: .light, text: text)
+    Quack.shared.hoverWindow.makeKeyAndVisible()
+  }
+  
+  /**
+   Present dark transparent hover with spinner
+   */
+  open class func showDarkHover() {
+    Quack.shared.hoverWindow.rootViewController = HoverViewController(hoverStyle: .dark)
+    Quack.shared.hoverWindow.makeKeyAndVisible()
+  }
+  
+  /**
+   Present light transparent hover with spinner and text
+   */
+  open class func showDarkHover(text: String) {
+    Quack.shared.hoverWindow.rootViewController = HoverViewController(hoverStyle: .dark, text: text)
+    Quack.shared.hoverWindow.makeKeyAndVisible()
+  }
+  
+  /**
+   Instantly hide hover
+   */
+  open class func hideHover() {
+    Quack.shared.hoverWindow.isHidden = true
+    UIApplication.shared.delegate?.window??.makeKeyAndVisible()
+  }
+  
+  /**
+   Hide hover with animation
+   */
+  open class func hideHover(animated: Bool) {
+    if animated {
+      if let hoverViewController = Quack.shared.hoverWindow.rootViewController as? HoverViewController {
+        hoverViewController.animateHide()
+        return
+      }
     }
-    
-    /**
-        Present light transparent hover with spinner
-    */
-    open class func showLightHover() {
-        Quack.shared.hoverWindow.rootViewController = HoverViewController(hoverStyle: .light)
-        Quack.shared.hoverWindow.makeKeyAndVisible()
-    }
-    
-    /**
-        Present light transparent hover with spinner and text
-    */
-    open class func showLightHover(text: String) {
-        Quack.shared.hoverWindow.rootViewController = HoverViewController(hoverStyle: .light, text: text)
-        Quack.shared.hoverWindow.makeKeyAndVisible()
-    }
-    
-    /**
-        Present dark transparent hover with spinner
-    */
-    open class func showDarkHover() {
-        Quack.shared.hoverWindow.rootViewController = HoverViewController(hoverStyle: .dark)
-        Quack.shared.hoverWindow.makeKeyAndVisible()
-    }
-    
-    /**
-        Present light transparent hover with spinner and text
-    */
-    open class func showDarkHover(text: String) {
-        Quack.shared.hoverWindow.rootViewController = HoverViewController(hoverStyle: .dark, text: text)
-        Quack.shared.hoverWindow.makeKeyAndVisible()
-    }
-    
-    /**
-        Instantly hide hover
-    */
-    open class func hideHover() {
-        Quack.shared.hoverWindow.isHidden = true
-        UIApplication.shared.delegate?.window??.makeKeyAndVisible()
-    }
-    
-    /**
-        Hide hover with animation
-    */
-    open class func hideHover(animated: Bool) {
-        if animated {
-            if let hoverViewController = Quack.shared.hoverWindow.rootViewController as? HoverViewController {
-                hoverViewController.animateHide()
-                return
-            }
-        }
-        self.hideHover()
-    }
+    self.hideHover()
+  }
 }
 
 public extension UIViewController {
-    
-    /**
-        Present UIAlertController with title, message and "OK" button from current controller
-    */
-    public func showAlert(title: String?, message: String?) {
-        Quack.showAlert(title: title, message: message, inController: self)
-    }
+  
+  /**
+   Present UIAlertController with title, message and "OK" button from current controller
+   */
+  public func showAlert(title: String?, message: String?) {
+    Quack.showAlert(title: title, message: message, inController: self)
+  }
 }
